386d9ecb859ca1f731888937e5a96a19ad1ffeed
최은영 <76518850+silver0-stack@users.noreply.github.com>
2023-06-29 16:11:30 +0900

    dd

--- Desktop/mumul 2/mumul/src/pages/Main.js
+++ Desktop/mumul 2/mumul/src/pages/Main.js
@@ -1,24 +1,28 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../component/Header";
 import SendCommnet from "../component/SendCommnet";
 import ReceiveComment from "../component/ReciveComment";
 import MyProfile from "../component/MyProfile";
 import QuestionerProfile from "../component/QuestionerProfile";
 import Storyslide from "../component/Storyslide";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getSpaceInfo } from "../api/getSpaceInfo";
 import { getUserInfo } from "../api/getUserInfo";
+// import QuestionerProfile from "../component/QuestionerProfile";
 
-function Main({isLogin}) {
-  const {id} = useParams();
+function Main({ isLogin }) {
+  const { id } = useParams();
+  const navigate = useNavigate();
   const [info, setInfo] = useState({
-    userId: '',
-    picture: '',
-    name: '',
+    userId: "",
+    picture: "",
+    name: "",
   });
   const [currentUserInfo, setCurrentUserInfo] = useState({
-    userId: '',
+    userId: "",
   });
+
+  const [isOwner, setIsOwner] = useState(false);
 
   useEffect(() => {
     const initUserInfo = async () => {
@@ -27,11 +31,21 @@ function Main({isLogin}) {
       setCurrentUserInfo(userInfo);
       setInfo(newInfo);
     };
+    
     initUserInfo();
-  }, []);
+    
+    if (!currentUserInfo) {
+      setIsOwner(false);
+    } else if (currentUserInfo.userId === info.userId) {
+      setIsOwner(true);
+    } else {
+      setIsOwner(false);
+    }
+  }, [isLogin]);
 
   console.log(currentUserInfo.userId);
   console.log(info.userId);
+  console.log(isOwner);
 
   const [activeIndex, setActiveIndex] = useState(0);
 
@@ -51,14 +65,19 @@ function Main({isLogin}) {
   // 유저의 고유 아이디를 사용하여 매핑
   return (
     <div className="wrap">
+
       <Header isLogin={isLogin}></Header>
+
       <div className="contentWrap">
+
         <Storyslide></Storyslide>
-        {currentUserInfo.userId === info.userId ? (
-            <MyProfile name={info.name} picture={info.picture}></MyProfile>
-          ) : (
-            <QuestionerProfile name={info.name} picture={info.picture}></QuestionerProfile>
+
+        {isOwner ? (
+          <MyProfile name={info.name} picture={info.picture}></MyProfile>
+        ) : (
+          <QuestionerProfile></QuestionerProfile>
         )}
+
         <ul className="tabMenu">
           {tabContArr.map((item) => (
             <li
@@ -70,6 +89,7 @@ function Main({isLogin}) {
             </li>
           ))}
         </ul>
+
         <div>
           {tabContArr
             .filter((item) => activeIndex === item.id)
