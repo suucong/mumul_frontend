61b814755f0fcb13ca4e96e242f995a6a2c5e0ad
suucong <sophia021210@naver.com>
2023-06-30 11:26:18 +0900

    fix: 프로필 수정에서 사진 가져왔을 때 바로 프로필 사진 바뀌도록, 프로필 동그라미로

--- mumul/src/component/popup/ProfileEdit.js
+++ mumul/src/component/popup/ProfileEdit.js
@@ -1,15 +1,26 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Close from "../../img/icon/close.png";
 import MyprofileImg from "../../img/Ellipse 104.png";
 import { putUserProfileEdit } from "../../api/putUserProfileEdit";
 
-function ProfileEdit({ onClose, userId, name, picture, introduce, instaId }) {
+function ProfileEdit({ onClose, userId, name, picture, introduce, instaId, link }) {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [previewUrl, setPreviewUrl] = useState(picture);
   const nicknameInput = useRef();
   const introduceInput = useRef();
   const snsInput = useRef();
   const linkInput = useRef();
   const imageInput = useRef();
+
+  useEffect(() => {
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setPreviewUrl(reader.result);
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  }, [selectedFile]);
 
   const onClickInput = () => {
     imageInput.current.click();
@@ -17,7 +28,7 @@ function ProfileEdit({ onClose, userId, name, picture, introduce, instaId }) {
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
-  }
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -28,21 +39,21 @@ function ProfileEdit({ onClose, userId, name, picture, introduce, instaId }) {
     const data = {
       info: {
         id: userId,
-        name: nicknameInput.current.value, 
+        name: nicknameInput.current.value,
         introduce: introduceInput.current.value,
         instaId: snsInput.current.value,
         link: linkInput.current.value,
-      }
+      },
     };
 
     formData.append(
       "requestDto",
-      new Blob([JSON.stringify(data.info)], {type: "application/json"})
+      new Blob([JSON.stringify(data.info)], { type: "application/json" })
     );
 
     const response = putUserProfileEdit(userId, formData);
     console.log(response.data);
-  }
+  };
 
   return (
     <div className="popupWrap">
@@ -54,7 +65,11 @@ function ProfileEdit({ onClose, userId, name, picture, introduce, instaId }) {
           <div className="profilePopup">
             <form onSubmit={handleSubmit}>
               <div className="profileArea">
-                <img src={picture} alt="myprofile" />
+                {previewUrl ? (
+                  <img src={previewUrl} alt="myprofile" />
+                ) : (
+                  <img src={picture} alt="myprofile" />
+                )}
                 <span className="editTxt" onClick={onClickInput}>
                   Edit
                 </span>
@@ -90,7 +105,7 @@ function ProfileEdit({ onClose, userId, name, picture, introduce, instaId }) {
                     ref={snsInput}
                   />
                   <label htmlFor="link">링크</label>
-                  <input type="text" id="link" ref={linkInput} />
+                  <input type="text" id="link" ref={linkInput} defaultValue={link}/>
                 </div>
                 <div className="btn">
                   <button className="editProfile active" type="submit" onClick={() => window.location.reload()}>
