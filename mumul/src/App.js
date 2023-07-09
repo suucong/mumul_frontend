35ae96a939866526872d42300f908eda2e1ab25e
최은영 <76518850+silver0-stack@users.noreply.github.com>
2023-07-09 11:07:26 +0900

    remove: redundant code

--- mumul/src/App.js
+++ mumul/src/App.js
@@ -19,53 +19,7 @@ import "./css/style.css";
 function App() {
   const [isLogin, setIsLogin] = useState(false);
   const [followSelected, setFollowSelected] = useState(true);
-  const [questionShareData, setQuestionShareData] = useState(null);
-  const [currentUserInfo, setCurrentUserInfo] = useState(null); // currentUserInfo 초기화
-  const questionId = localStorage.getItem('questionId');
-
-  const QuestionShareRoute = () => {
-
-    useEffect(() => {
-      const fetchQuestionShareData =  () => {
-        try {
-          const data =  getQuestionShare(questionId);
-          setQuestionShareData(data);
-        } catch (error) {
-          console.error("Error fetching question share data:", error);
-        }
-      };
-
-      fetchQuestionShareData();
-    });
-
-  // questionShareData와 currentUserInfo를 이용해 데이터를 렌더링하는 컴포넌트를 반환합니다.
-  return questionShareData && currentUserInfo ? (
-    <ShareComment
-      questionShareData={questionShareData}
-      currentUserInfo={currentUserInfo}
-    />
-  ) : null;
-};
-
-
-useEffect(() => {
-  const fetchUserInfo = async () => {
-    try {
-      // 로그인 상태에 따라 currentUserInfo를 설정합니다.
-      if (isLogin) {
-        // 로그인된 사용자 정보를 가져와서 currentUserInfo를 설정합니다.
-        const userInfo = await getUserInfo(); // getUserInfo는 사용자 정보를 가져오는 함수로 적절하게 대체해야 합니다.
-        setCurrentUserInfo(userInfo);
-      } else {
-        setCurrentUserInfo(null); // 로그아웃 상태라면 currentUserInfo를 null로 설정합니다.
-      }
-    } catch (error) {
-      console.error("Error fetching user info:", error);
-    }
-  };
-
-  fetchUserInfo();
-}, [isLogin]);
+  
 
 
   return (
@@ -80,10 +34,6 @@ useEffect(() => {
         <Route path="/main2" element={<Main2 />} />
         <Route path="/policy" element={<Policy />} />
         <Route path="/setting" element={<Setting isLogin={isLogin} setIsLogin={setIsLogin}/>} />
-        <Route
-          path={`/spaces/${questionId}/get`}
-          element={<QuestionShareRoute />}
-        />
         <Route path="*" element={<div>404</div>} />
       </Routes>
     </BrowserRouter>
