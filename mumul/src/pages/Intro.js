d7790ee98e9c6b03fd767586f5b5a012ae4b0af1
suucong <sophia021210@naver.com>
2023-07-12 18:06:47 +0900

    ..

--- mumul/src/pages/Intro.js
+++ mumul/src/pages/Intro.js
@@ -31,7 +31,7 @@ const Intro = () => {
         <p className="introTitle">🐇토끼🐇로 무물에 녹아 들자</p>
         <Comment></Comment>
         <Link
-          to={isLogin ? `/space/${currentUserInfo.userId}` : "/login"}
+          to={isLogin ? `/${currentUserInfo.userId}` : "/login"}
           className="goSpace">
           <button className="space">스페이스 입장</button>
         </Link>
