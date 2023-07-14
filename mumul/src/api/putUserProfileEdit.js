ff7edd8c1a382e612e7ef7b06eacb8ee1b1465dc
suucong <sophia021210@naver.com>
2023-07-14 16:10:39 +0900

    프로필 수정

--- mumul/src/api/putUserProfileEdit.js
+++ mumul/src/api/putUserProfileEdit.js
@@ -4,14 +4,14 @@ export const putUserProfileEdit = async (currentUserId, formData) => {
     const path = '/v1/oauth/user/update/' + currentUserId;
 
     try {
-        const response = await axios.put(path, formData, {
+        const response = await axios.put(path, {
             headers: {
               'Accept': 'application/json',
               'Content-Type': 'application/json',
             },
             withCredentials: true,
             crossDomain: true,
-          });
+          }, formData);
         return response.data;
     } catch(error) {
         console.error(error);
