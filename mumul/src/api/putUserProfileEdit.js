b29e79f25a4b77d246a3273d21cf073969a0278e
suucong <sophia021210@naver.com>
2023-07-14 16:30:11 +0900

    ..

--- mumul/src/api/putUserProfileEdit.js
+++ mumul/src/api/putUserProfileEdit.js
@@ -4,15 +4,7 @@ export const putUserProfileEdit = async (currentUserId, formData) => {
     const path = '/v1/oauth/user/update/' + currentUserId;
 
     try {
-        const response = await axios.put(path, formData, {
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-              Authorization: 'Bearer ' + localStorage.getItem('token')
-            },
-            withCredentials: true,
-            crossDomain: true,
-          });
+        const response = await axios.put(path, FormData);
         
         console.log(response.data);
         return response.data;
