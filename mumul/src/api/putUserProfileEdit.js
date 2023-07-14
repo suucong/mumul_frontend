2f2dcf51dd4f14f5f7dabeaaf761110ff1cb6b4e
suucong <sophia021210@naver.com>
2023-07-14 17:18:09 +0900

    put 메소드 헤더에 토큰 같이 보내기

--- mumul/src/api/putUserProfileEdit.js
+++ mumul/src/api/putUserProfileEdit.js
@@ -4,9 +4,14 @@ export const putUserProfileEdit = async (currentUserId, formData) => {
     const path = '/v1/oauth/user/update/' + currentUserId;
 
     try {
-        const response = await axios.put(path, formData);
+        const response = await axios.put(path, formData, {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: 'Bearer ' + localStorage.getItem('token')
+          }
+      });
         
-        console.log(response.data);
         return response.data;
     } catch(error) {
         console.error(error);
