79d31b14ee8af09c1ac280df2e724459bc1655d8
suucong <sophia021210@naver.com>
2023-07-14 17:02:28 +0900

    test

--- mumul/src/component/popup/ProfileEdit.js
+++ mumul/src/component/popup/ProfileEdit.js
@@ -118,7 +118,6 @@ function ProfileEdit({ onClose, currentUserInfo }) {
                     type="submit"
                     onClick={(event) => {
                       handleSubmit(event); // event 객체를 전달하여 handleSubmit 함수 호출
-                      setTimeout(() => window.location.reload(), 1000);
                     }}
                   >
                     수정하기
