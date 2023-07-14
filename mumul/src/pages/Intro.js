94e657ca0486bea4edcf4b2043dc8bc6f4c9a4cb
suucong <sophia021210@naver.com>
2023-07-14 18:21:33 +0900

    인트로 임시 수정

--- mumul/src/pages/Intro.js
+++ mumul/src/pages/Intro.js
@@ -2,10 +2,8 @@ import { Link } from "react-router-dom";
 import Comment from "../component/Comment";
 import React, { useState, useEffect } from "react";
 import { getUserInfo } from "../api/getUserInfo";
-import { useNavigate } from "react-router-dom";
 
 const Intro = () => {
-  const navigate = useNavigate();
   const [isLogin, setIsLogin] = useState(false);
   const [currentUserInfo, setCurrentUserInfo] = useState({
     userId: '',
@@ -27,24 +25,14 @@ const Intro = () => {
     initUserInfo();
   }, []);
 
-  const handleIntroSpace = () => {
-    const response = getUserInfo();
-    if(response === false) {
-      navigate('/login');
-      return;
-    } else {
-      setCurrentUserInfo(response);
-      navigate(`/${currentUserInfo.userId}`);
-      setIsLogin(true);
-    }
-  }
-
   return (
     <div className="wrap intro">
       <div className="contentWrap">
         <p className="introTitle">🐇토끼🐇로 무물에 녹아 들자</p>
         <Comment></Comment>
-        <button className="space" onClick={handleIntroSpace}>스페이스 입장</button>
+        <Link to="/login" className="goSpace">
+          <button className="space">스페이스 입장</button>
+        </Link>
         <Link to="/policy" className="goPolicy">
           <p>PRIVACY POLICY</p>
         </Link>
